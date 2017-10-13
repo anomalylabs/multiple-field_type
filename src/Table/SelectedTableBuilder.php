b83f0607c3b7f17219cad2d03a585aaa71a23669
Denis Efremov <efremov.a.denis@gmail.com>
2017-10-13 08:06:46 +0300

    Update SelectedTableBuilder.php

--- src/Table/SelectedTableBuilder.php
+++ src/Table/SelectedTableBuilder.php
@@ -172,10 +172,16 @@ class SelectedTableBuilder extends TableBuilder
      */
     public function setTableEntries(\Illuminate\Support\Collection $entries)
     {
-        if (!$this->getFieldType()) {
+        if (!$fieldType = $this->getFieldType()) {
             $entries = $entries->sort(
                 function ($a, $b) {
-                    return array_search($a->id, $this->getSelected()) - array_search($b->id, $this->getSelected());
+                    return array_search(
+                        $a->{$this->config('key_name', 'id')},
+                        $this->getSelected()
+                    ) - array_search(
+                        $b->{$this->config('key_name', 'id')},
+                        $this->getSelected()
+                    );
                 }
             );
         }
